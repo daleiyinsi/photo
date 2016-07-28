176809559f74d62a585cd9caee8b20a7a81e48a2
Lishanfeng <shan_1023@163.com>
2016-07-28 17:06:38 +0800

    commit

---
+++ photo/app/src/main/java/com/li/shanfeng/photo/image/ImagePagerActivity.java
@@ -0,0 +1,141 @@
+package com.li.shanfeng.photo.image;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+
+import com.li.shanfeng.photo.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImagePagerActivity extends FragmentActivity {
+    private static final String STATE_POSITION = "STATE_POSITION";
+    public static final String EXTRA_IMAGE_INDEX = "image_index";
+    public static final String EXTRA_IMAGE_URLS = "image_urls";
+    private HackyViewPager mPager;
+    private TextView indicator;
+    private int pagerPosition;
+    private List<String> imageUrl = new ArrayList<String>(); // 滑动的图片集合
+    private Context mContext;
+    private Bundle savedInstanceState;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //去掉Activity上面的状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.image_detail_pager);
+        setTranslucentStatus();
+        this.savedInstanceState = savedInstanceState;
+        mContext = ImagePagerActivity.this;
+        Bundle bundle = getIntent().getExtras();
+        pagerPosition = getIntent().getIntExtra(EXTRA_IMAGE_INDEX, 0);
+
+        imageUrl = getIntent().getStringArrayListExtra("pics");
+        reqBitmap();
+
+    }
+
+    /**
+     * 设置状态栏背景状态
+     */
+    private void setTranslucentStatus() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window win = getWindow();
+            WindowManager.LayoutParams winParams = win.getAttributes();
+            final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            winParams.flags |= bits;
+            win.setAttributes(winParams);
+        }
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+
+        tintManager.setStatusBarTintEnabled(false);
+        tintManager.setStatusBarTintResource(0);// 状态栏无背景
+    }
+
+    public void reqBitmap() {
+        if (imageUrl == null) {
+            return;
+        }
+        final int size = imageUrl.size();
+        String[] urls = (String[]) imageUrl.toArray(new String[size]);// list转数组
+
+        mPager = (HackyViewPager) findViewById(R.id.pager);
+        ImagePagerAdapter mAdapter = new ImagePagerAdapter(getSupportFragmentManager(), urls);
+        mPager.setAdapter(mAdapter);
+        indicator = (TextView) findViewById(R.id.indicator);
+
+
+        CharSequence text = getString(R.string.viewpager_indicator, 1, mPager.getAdapter().getCount());
+        indicator.setText(text);
+        // 更新下标
+        mPager.addOnPageChangeListener(new OnPageChangeListener() {
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+
+            @Override
+            public void onPageSelected(int arg0) {
+                CharSequence text = getString(R.string.viewpager_indicator, arg0 + 1, mPager.getAdapter().getCount());
+                indicator.setText(text);
+            }
+
+        });
+
+        if (savedInstanceState != null) {
+            pagerPosition = savedInstanceState.getInt(STATE_POSITION);
+        }
+
+        mPager.setCurrentItem(pagerPosition);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(STATE_POSITION, mPager.getCurrentItem());
+    }
+
+    private class ImagePagerAdapter extends FragmentStatePagerAdapter {
+
+        public String[] fileList;
+
+        public ImagePagerAdapter(FragmentManager fm, String[] fileList) {
+            super(fm);
+            this.fileList = fileList;
+        }
+
+        @Override
+        public int getCount() {
+            return fileList == null ? 0 : fileList.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            String url = fileList[position];
+            return ImageDetailFragment.newInstance(url);
+        }
+
+    }
+
+
+}
